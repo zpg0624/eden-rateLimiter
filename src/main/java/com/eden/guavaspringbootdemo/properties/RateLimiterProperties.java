704fbc61421bea94587585bab108aedca5edb9b1
pinggen <ksxx2016>
2019-04-16 09:21:19 +0800

    添加Springboot config annotation processor

--- src/main/java/com/eden/guavaspringbootdemo/properties/RateLimiterProperties.java
+++ src/main/java/com/eden/guavaspringbootdemo/properties/RateLimiterProperties.java
@@ -2,7 +2,6 @@ package com.eden.guavaspringbootdemo.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @created by eden
